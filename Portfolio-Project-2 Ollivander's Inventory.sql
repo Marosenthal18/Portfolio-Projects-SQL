f0316ca1a7c20a7a115b70a5bcbc22fd6ccb64d7
Matthew Rosenthal <60559647+Marosenthal18@users.noreply.github.com>
2022-10-31 22:33:26 -0400

    Update Portfolio-Project-2 Ollivander's Inventory.sql

--- Portfolio-Project-2 Ollivander's Inventory.sql
+++ Portfolio-Project-2 Ollivander's Inventory.sql
@@ -16,3 +16,23 @@ on w.code = wp.code
 where wp.is_evil = 0
 group by wp.age, w.power)
 order by w.power desc, wp.age desc
+
+
+-- Same thing, but with a window function
+
+SELECT id, age, coins_needed, power
+FROM
+    (SELECT 
+        W.id,
+        Wp.age,
+        W.coins_needed,
+        W.power,
+        DENSE_RANK() OVER(
+            PARTITION BY W.power, Wp.age
+            ORDER BY W.coins_needed
+        ) power_age_rank
+    FROM Wands W
+    JOIN Wands_Property Wp  ON W.code=Wp.code AND Wp.is_evil=0
+) d 
+WHERE power_age_rank=1
+ORDER BY power DESC, age DESC
