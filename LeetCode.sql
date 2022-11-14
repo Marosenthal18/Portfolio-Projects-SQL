7ce965438986f757325668e90f25a1f94c0b5563
Matthew Rosenthal <60559647+Marosenthal18@users.noreply.github.com>
2022-11-14 13:51:25 -0500

    Update LeetCode.sql

--- LeetCode.sql
+++ LeetCode.sql
@@ -25,3 +25,27 @@ JOIN Department d ON e.departmentId = d.id
 ) d 
 
 where rnk = 1
+
+
+--Finding the fraction of players who logged into this video game for at least two_consecutive days 
+--CTEs, Joins, Dates
+
+with first_logins AS (select 
+
+a.player_id,
+min(a.event_date) first_login
+
+from Activity a 
+group by a.player_id),
+
+
+conseq_logins as (select 
+count(a.player_id) AS num_logins
+
+from first_logins f 
+JOIN Activity a ON f.player_id = a.player_id 
+AND f.first_login = date_sub(a.event_date, INTERVAL 1 DAY))
+
+select 
+
+round((select num_logins from conseq_logins) / (select count(player_id) from first_logins) ,2 ) fraction 
